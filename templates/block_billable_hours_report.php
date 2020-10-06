fb872bac2d6568ca4d1d68f4c153c357b1120bde
Casey Milne <casey.milne@eatbuildplay.com>
2020-10-05 22:02:32 -0500

    adding export file for fields

--- templates/block_billable_hours_report.php
+++ templates/block_billable_hours_report.php
@@ -1,1 +1,24 @@
-HELLO BLOCK TEMPAplAHl 22343
+<?php
+
+global $post;
+
+$endDate = get_field('end_date', $post->ID);
+$period = get_field('period', $post->ID);
+$billableHours = get_field('billable_hours', $post->ID);
+
+?>
+
+<table>
+  <tr>
+    <td>Period</td>
+    <td><?php print $period; ?></td>
+  </tr>
+  <tr>
+    <td>End Date</td>
+    <td><?php print $endDate; ?></td>
+  </tr>
+  <tr>
+    <td>Billable Hours</td>
+    <td><?php print $billableHours; ?></td>
+  </tr>
+</table>
